cfd8a02effa4c33b30a3e26ead96cd396f34d819
mpurnell133 <mark@markpurnell.co.uk>
2013-02-14 10:41:04 +0000

    removed the alert, previous searched are now 'removed' properly

--- js/guardian.js
+++ js/guardian.js
@@ -1,7 +1,6 @@
 
 function getFeed(){
     var numResults = $('#numResults').val();
-    alert(numResults);
     var query = $('#query').val();
     var url = ('http://content.guardianapis.com/search?q='+query+'&format=json&show-fields=thumbnail%2Cstandfirst&page-size='+numResults+'&callback=?&api-key=ryj6zc777unnpambw8rdsnj3');
 
@@ -17,10 +16,11 @@ function getFeed(){
         var feedlist = response.response.results;
         if($("article")){
             $('article').remove();
+            $('hr').remove();
         }
         for(var i = 0; i < feedlist.length; i++){
             var article = $('<article></article>');
-            var title = $('<h1>'+feedlist[i].webTitle+'</h1>');
+            var title = $('<h2>'+feedlist[i].webTitle+'</h2>');
             var brk = $('<br />');
             var lbrk = $('<hr />');
             var thumbnail = $('<img src="'+feedlist[i].fields.thumbnail+'"></img>');
@@ -28,7 +28,8 @@ function getFeed(){
             var link = $('<a>'+feedlist[i].webUrl+'</a>');
             var foota = $('<footer> Posted on '+feedlist[i].webPublicationDate+'</footer>')
             $(feedWrapper).append(article);
-            $(article).append(title, summary, thumbnail, brk, link, foota, lbrk);
+            $(article).append(title, summary, thumbnail, brk, link, foota);
+            $(feedWrapper).append(lbrk);
         }
     });
 }
