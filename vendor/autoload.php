20bf6ecd3af5f8ca2d4a15f7ac1c1df125dfc2ce
Ivan <ivantashev2001@yahoo.com>
2013-07-25 13:14:39 +0100

    more files added by the composser

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd88cc5b89b8cfaf5883ba5eba4d565f8::getLoader();
+return ComposerAutoloaderInit070a37a4659330ce0bb26a40fab2f7a3::getLoader();
