0d0eaf4c398a3d2ea7c4477302d91eb36e45a4c1
Ivan <ivantashev2001@yahoo.com>
2013-07-25 16:16:01 +0100

    Added font-awesome to projejct

--- web/index.php
+++ web/index.php
@@ -13,6 +13,8 @@ use Symfony\Component\Debug\Debug;
             <script type="text/javascript" src="js/bootstrap.min.js"></script>
             <script type="text/javascript" src="js/custom.js"></script>
             <link href="css/bootstrap.min.css" rel="stylesheet">
+            <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+            <link href="css/font-awesome.min.css" rel="stylesheet">
             <link href="css/theme.css" rel="stylesheet">
             <link href="css/custom.css" rel="stylesheet">
     </head>
