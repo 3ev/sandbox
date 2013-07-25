6dab386ea3d4f9663bd49f43fcf0e3f7f5bee038
Ivan <ivantashev2001@yahoo.com>
2013-07-25 09:28:32 +0100

    Added custom.js where all custom js code will be added.

--- index.php
+++ index.php
@@ -19,33 +19,15 @@
         <div class="container">
                 <fieldset>
                     <div class="control-group">
-                        <p>
-                            Write a PHP program that generates an XML file containing every 30th of June since the Unix Epoch, at 1pm GMT, similar to the one attached.
-                        </p>
-                        <p>
-                            The program must also parse the generated XML file and generate a second XML file sorted by timestamp in descending order, excluding years that are prime numbers. The timestamps generated should be at 1pm PST.
-                        </p>
-                        <p>
-                            We must be able to run these steps separately. 
-                        </p>
-                        <p>
-                            Remember, you need to solve the problem but also show us your knowledge of professional PHP coding and OOP. The code must be written to production standard - the kind of thing you'd expect from anyone you work with. And it has to run first time!
-                        </p>
+                       
                     </div>
                     <div class="control-group">
                       <div class="controls">
                             <button class="btn btn-success generate">Generate xml</button>
                       </div>
-                      <div class="xmlLink">
-                      </div>
                     </div>
                     <div class="control-group sortxmlWrapper">
-                        <label class="control-label"  for="name">Second step</label>
-                        <div class="controls ">
-                            <button class="btn btn-success sortxml">Sort XML file</button>
-                        </div>
-                        <div class="controls sortedxml">
-                        </div>
+                        
                     </div>
                 </fieldset>
         </div>
